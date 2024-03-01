a9b2d402bff405ae14f17cb909d617107a20c564
Solomon Mensah <solomens522007@gmail.com>
2024-03-01 11:33:54 +0000

    Add AppUtils.java

---
+++ app/src/main/java/com/itsmcodez/playful/utils/AppUtils.java
@@ -0,0 +1,15 @@
+package com.itsmcodez.playful.utils;
+import android.util.Log;
+import java.util.ArrayList;
+
+public class AppUtils {
+    public static final String TAG = "AppUtils";
+    private static ArrayList<String> registrars = new ArrayList<>();
+    
+    public AppUtils(){}
+    
+    public static void addRegistrar(String registrar) {
+        Log.d(TAG, "Adding " + registrar);
+    	registrars.add(registrar);
+    }
+}
