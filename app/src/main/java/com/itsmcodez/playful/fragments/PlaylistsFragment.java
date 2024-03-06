b78d5d0decbf9cb12d24f7d1cfb8e63f26a35630
Solomon Mensah <solomens522007@gmail.com>
2024-03-06 12:34:52 +0000

    Update PlaylistsFragment (setup viewmodel)

--- app/src/main/java/com/itsmcodez/playful/fragments/PlaylistsFragment.java
+++ app/src/main/java/com/itsmcodez/playful/fragments/PlaylistsFragment.java
@@ -10,14 +10,29 @@ import android.view.View;
 import android.widget.Toast;
 import androidx.appcompat.app.AlertDialog;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.GridLayoutManager;
 import com.google.android.material.dialog.MaterialAlertDialogBuilder;
 import com.itsmcodez.playful.R;
+import com.itsmcodez.playful.adapters.PlaylistsAdapter;
 import com.itsmcodez.playful.databinding.FragmentPlaylistsBinding;
 import com.itsmcodez.playful.databinding.LayoutAddPlaylistBinding;
+import com.itsmcodez.playful.models.PlaylistsModel;
+import com.itsmcodez.playful.viewmodels.PlaylistsViewModel;
+import java.util.ArrayList;
 
 public class PlaylistsFragment extends Fragment {
     private FragmentPlaylistsBinding binding;
     private static final String TAG = "PlaylistsFragment";
+    private PlaylistsAdapter playlistsAdapter;
+    private PlaylistsViewModel playlistsViewModel;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        playlistsViewModel = new ViewModelProvider(this).get(PlaylistsViewModel.class);
+    }
 
     @Override
     public View onCreateView(
@@ -28,16 +43,34 @@ public class PlaylistsFragment extends Fragment {
         // Add a new playlist
         binding.fabAdd.setOnClickListener(
                 view -> {
-                    showAddPlaylistDialog(
-                            container.getContext());
+                    showAddPlaylistDialog(container.getContext());
                 });
+
+        // RecyclerView
+        binding.recyclerView.setLayoutManager(new GridLayoutManager(container.getContext(), 2));
+
+        // Observe LiveData (playlistsViewModel)
+        playlistsViewModel
+                .getAllPlaylists()
+                .observe(
+                        getViewLifecycleOwner(),
+                        new Observer<ArrayList<PlaylistsModel>>() {
+                            @Override
+                            public void onChanged(ArrayList<PlaylistsModel> allPlaylists) {
+                                
+                                playlistsAdapter = new PlaylistsAdapter(container.getContext(), inflater, allPlaylists);
+                                binding.recyclerView.setAdapter(playlistsAdapter);
+                                
+                            }
+                        });
 
         return binding.getRoot();
     }
 
     private void showAddPlaylistDialog(final Context context) {
-        
-        LayoutAddPlaylistBinding layoutAddPlaylistBinding = LayoutAddPlaylistBinding.inflate(getLayoutInflater());
+
+        LayoutAddPlaylistBinding layoutAddPlaylistBinding =
+                LayoutAddPlaylistBinding.inflate(getLayoutInflater());
 
         AlertDialog addPlaylistDialog =
                 new MaterialAlertDialogBuilder(context)
@@ -49,11 +82,25 @@ public class PlaylistsFragment extends Fragment {
                                 new DialogInterface.OnClickListener() {
                                     @Override
                                     public void onClick(DialogInterface dialog, int i) {
-                                        if(layoutAddPlaylistBinding.textInputEditText.getText().toString().trim().isEmpty()){
-                                            Toast.makeText(context, R.string.fragment_playlists_dialog_positive_bt_clicked_rational, Toast.LENGTH_LONG).show();
-                                        }
-                                        else{
-                                            String title = layoutAddPlaylistBinding.textInputEditText.getText().toString();
+                                        if (layoutAddPlaylistBinding
+                                                .textInputEditText
+                                                .getText()
+                                                .toString()
+                                                .trim()
+                                                .isEmpty()) {
+                                            Toast.makeText(
+                                                            context,
+                                                            R.string
+                                                                    .fragment_playlists_dialog_positive_bt_clicked_rational,
+                                                            Toast.LENGTH_LONG)
+                                                    .show();
+                                        } else {
+                                            String title =
+                                                    layoutAddPlaylistBinding
+                                                            .textInputEditText
+                                                            .getText()
+                                                            .toString();
+                                            playlistsViewModel.addPlaylist(title);
                                             Log.d(TAG, "Added " + title);
                                         }
                                     }
@@ -68,12 +115,12 @@ public class PlaylistsFragment extends Fragment {
                                 })
                         .create();
         addPlaylistDialog.setOnCancelListener(
-                                new DialogInterface.OnCancelListener() {
-                                    @Override
-                                    public void onCancel(DialogInterface dialog) {
-                                        Toast.makeText(context, R.string.cancel, Toast.LENGTH_SHORT).show();
-                                    }
-                                });
+                new DialogInterface.OnCancelListener() {
+                    @Override
+                    public void onCancel(DialogInterface dialog) {
+                        Toast.makeText(context, R.string.cancel, Toast.LENGTH_SHORT).show();
+                    }
+                });
         addPlaylistDialog.show();
     }
 
@@ -82,8 +129,6 @@ public class PlaylistsFragment extends Fragment {
         super.onDestroyView();
         this.binding = null;
     }
-    
-    private void addPlaylist() {
-    	
-    }
+
+    private void addPlaylist() {}
 }
